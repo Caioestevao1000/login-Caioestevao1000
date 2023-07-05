4f7a0e05ac8f31787da82bcb2319019c45c3107b
caioestevao1000 <caioestevao_@hotmail.com>
2023-07-05 08:58:57 -0300

    js and html

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ function Verify()
     let user = document.getElementById("loginEmail").value;
     let pswr = document.getElementById("loginSenha").value;
 
-    if (!user || !pswr)
-    {
+    if (!user || !pswr){
         alert("Campos de preenchimento obrigatório. Favor preencher!");
     }
-    else
-    {
-        alert("Campos preenchidos com sucesso!");
+    else{
+        window.location.href = "aula2.html";
     }
 }
